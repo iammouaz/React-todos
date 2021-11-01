38fb45b924dead139ac7a32bbb6040b9c3284ee2
iammouaz <88675267+iammouaz@users.noreply.github.com>
2021-11-01 22:23:25 +0200

    Add and remove method & Styling

---
+++ src/components/TodoItem.js
@@ -0,0 +1,29 @@
+import React from "react"
+import styles from "./TodoItem.module.css"
+
+const completedStyle = {
+    fontStyle: "italic",
+    color: "#595959",
+    opacity: 0.4,
+    textDecoration: "line-through",
+  }
+function TodoItem(props) {
+  return( <li className={styles.item}>
+<input
+  type="checkbox"
+  className={styles.checkbox}
+  checked={props.todo.completed}
+  onChange={() => props.handleChangeProps(props.todo.id)}
+/>
+<button onClick={() => props.deleteTodoProps(props.todo.id)}>
+  Delete
+</button>
+
+<span style={props.todo.completed ? completedStyle : null}>
+      {props.todo.title}
+    </span>
+   </li>
+   )
+}
+
+export default TodoItem
